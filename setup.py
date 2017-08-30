8bd21685a0266621e2a254a6dc6f02afe63492c7
Barnaby <barnaby@spotlightdata.co.uk>
2017-08-30 16:12:31 +0100

    minor updates to setup

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
+
+VERSION = None
+with open("VERSION") as f:
+    VERSION = f.read()
 
 setup(
-    name='sld',
-    version='0.1.0',
-    packages=['sld'],
-    requires=[
-        'pika',
-        'minio'
-    ]
+    name='nanowire-plugin-py',
+    version=VERSION,
+    packages=['nanowire_plugin'],
+    requires=['pika', 'minio'],
+    url="https://github.com/SpotlightData/nanowire-plugin-py",
+    author='Barnaby "Southclaws" Keene',
+    author_email='southclaws@gmail.com',
+    license='MIT'
 )
