bb9384af8976c9efc883dc2ab8f2c842bbe1c73d
Barnaby <barnaby@spotlightdata.co.uk>
2017-09-20 16:34:42 +0100

    ensured environ is being written to

--- tester/__init__.py
+++ tester/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import logging
+from os import environ
 
 from nanowire_plugin import bind
 
@@ -16,6 +17,12 @@ LOG.setLevel(logging.INFO)
 
 def entry(nmo: dict, jsonld: dict, source: str) -> dict:
     LOG.info("PLUGIN: %s:%s:%s", nmo["job"]["job_id"], nmo["task"]["task_id"], str(jsonld))
+
+    LOG.info(str(environ))
+
+    if "TEST_VAR" not in environ:
+        raise Exception("TEST_VAR not found in env vars")
+
     return {"@type": "TextDigitalDocument"}
 
 bind(entry, "extract")
