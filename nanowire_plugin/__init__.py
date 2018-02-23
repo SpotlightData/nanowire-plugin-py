94536da4ce261b2441f7e6fdd783d504c93231dd
stuart <stuart@spotlightdata.co.uk>
2018-02-23 12:54:51 +0000

    more python2/3 schneanagens

--- nanowire_plugin/__init__.py
+++ nanowire_plugin/__init__.py
@@ -395,12 +395,12 @@ def set_status(monitor_url, job_id, task_id, name, error=0):
     elif sys.version_info.major == 2:
         
         if not isinstance(job_id, unicode):
-            raise Exception("job_id should be a string, it is %s, a %s"%(str(job_id), str(type(job_id))))
+            raise Exception("job_id should be in unicode in python2, it is %s, a %s"%(str(job_id), str(type(job_id))))
         
         if not isinstance(task_id, unicode):
-            raise Exception("task_id should be a string, it is %s, a %s"%(str(task_id), str(type(task_id))))
+            raise Exception("task_id should be in unicode in python2, it is %s, a %s"%(str(task_id), str(type(task_id))))
         
-        if not isinstance(name, unicode):
+        if not isinstance(name, str):
             raise Exception("plugin name should be a string, it is %s, a %s"%(str(name), str(type(name))))
         
     
